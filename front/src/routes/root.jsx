11e2d7457b37717ce67c4c1d066df105e4653233
Zohair-coder <zohair.ul.hasan@gmail.com>
2024-04-16 11:51:18 -0400

    added react files

---
+++ front/src/routes/root.jsx
@@ -0,0 +1,12 @@
+export default function Root () {
+    return (
+        <>
+            <ul>
+                <li><a href="/1">First Item</a></li>
+                <li><a href="/2">Second Item</a></li>
+                <li><a href="/3">Third Item</a></li>
+                <li><a href="/4">Fourth Item</a></li>
+            </ul>
+        </>
+    )
+}
